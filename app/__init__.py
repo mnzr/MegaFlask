bbb35c723dcd4c1507cf9bb40008a78eb21a492d
Ratul Minhaz <minhaz.ratul@gmail.com>
2016-02-09 23:18:32 +0600

    Added database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 # Creates an object called app of Flask class
 app = Flask(__name__)
 app.config.from_object('config')
-# Imports app package from views, not the same as object 'app'
-from app import views
+db = SQLAlchemy(app)
 
+# Imports app package from views, not the same as object 'app'
+from app import views, models
